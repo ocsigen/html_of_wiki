4fad4b3af32c8ec662a666969476aa9ad18dca16
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-11-29 18:46:56 +0100

    Add a repository cloning script

---
+++ clone.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+find . -type d -depth 1 -regex ".*/[^.].*$" |
+cut -c 3- |
+while read line; do
+	REPOSITORY="git@github.com:ocsigen/$line"
+	echo Processing $line...
+	mkdir ../gen 2>/dev/null || true
+	cd ../gen
+	if [ -d "$line" ]; then
+		cd "$line"
+		if [ -d .git ]; then
+			git pull origin gh-pages
+			if [ `git diff --name-only --diff-filter=U |wc -l` -ne 0 ]; then
+				echo Please resolve conflicts in $line and press Ctrl-D...
+				$SHELL
+			fi
+		else
+			echo `pwd` is not a Git repository. 2>&1
+			echo Exiting due to hygiene violations. 2>&1
+			exit 1
+		fi
+		cd - >/dev/null
+	elif git clone -b gh-pages $REPOSITORY 2>/dev/null; then
+		true #nothing else to do!
+	else
+		mkdir ../gen/"$line"
+		cd ../gen/"$line"
+		git init
+		git checkout -b gh-pages
+		git remote add origin $REPOSITORY
+		cd - >/dev/null
+	fi
+done
