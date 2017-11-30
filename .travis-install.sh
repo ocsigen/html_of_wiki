74145d4f30f419ac0d1178c3c3be1276a0cdd6e5
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-11-30 17:31:20 +0100

    We still need opam

--- .travis-install.sh
+++ .travis-install.sh
@@ -3,13 +3,13 @@ OPAM_SWITCH=${OPAM_SWITCH:-system}
 FORK_USER=${FORK_USER:-ocsigen}
 
 
-if [ -d "$HOME/.opam" ]; then
-	exit 0 #do this just once
-fi
 sudo add-apt-repository -y ppa:avsm/ppa
 sudo apt-get update
 sudo apt-get install -y opam
 
+if [ -d "$HOME/.opam" ]; then
+	exit 0 #do this just once
+fi
 export OPAMYES=1
 opam init -a --comp="$OPAM_SWITCH"
 eval $(opam config env)
