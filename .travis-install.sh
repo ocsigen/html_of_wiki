4e74a822673c7d9174ec971b6e52f35c00570e13
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-11-30 17:28:18 +0100

    Use the cache

--- .travis-install.sh
+++ .travis-install.sh
@@ -3,6 +3,9 @@ OPAM_SWITCH=${OPAM_SWITCH:-system}
 FORK_USER=${FORK_USER:-ocsigen}
 
 
+if [ -d "$HOME/.opam" ]; then
+	exit 0 #do this just once
+fi
 sudo add-apt-repository -y ppa:avsm/ppa
 sudo apt-get update
 sudo apt-get install -y opam
