4126c62db1ba16f295a397bd3910df3d77473cc7
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-12-01 10:17:07 +0100

    Remove html_of_wiki from the cache!

--- .travis-install.sh
+++ .travis-install.sh
@@ -7,10 +7,11 @@ sudo add-apt-repository -y ppa:avsm/ppa
 sudo apt-get update
 sudo apt-get install -y opam
 
+export OPAMYES=1
 if [ -d "$HOME/.opam" ]; then
+	opam remove html_of_wiki
 	exit 0 #do this just once
 fi
-export OPAMYES=1
 opam init -a --comp="$OPAM_SWITCH"
 eval $(opam config env)
 opam pin add html_of_wiki https://github.com/$FORK_USER/html_of_wiki.git
