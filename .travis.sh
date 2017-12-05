40f00783f6f89306e786cf390bf30e878619f623
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-12-05 11:35:17 +0100

    Implement commit & push

--- .travis.sh
+++ .travis.sh
@@ -10,9 +10,21 @@ export SSH_KEY=
 
 
 eval $(opam config env)
-git clone --depth 1 https://github.com/ocsigen/ocsigen.org-data
-git clone --depth 1 https://github.com/ocsigen/tyxml
-cd tyxml
-OPAMYES=1 sh .jenkins.sh
+git clone --depth 1 https://github.com/ocsigen/ocsigen.org-data data
+git clone --depth 1 https://github.com/ocsigen/$PROJECT
+cd data
+git rm -rf ${PROJECT}/dev
+cd ../$PROJECT
+TARGET_DIR=../data/$PROJECT/dev
+API_DIR=${TARGET_DIR}/api
+MANUAL_SRC_DIR=${TARGET_DIR}/manual/src
+MANUAL_FILES_DIR=${TARGET_DIR}/manual/files
+mkdir -p ${API_DIR}
+mkdir -p ${MANUAL_SRC_DIR}
+mkdir -p ${MANUAL_FILES_DIR}
+OPAMYES=1 . .jenkins.sh
+do_build_doc
 
-echo TODO commit
+cd ../data
+git commit -m `git log -n 1 --format=$PROJECT-%h`
+git push 'git@github.com:ocsigen/ocsigen.org-data'
