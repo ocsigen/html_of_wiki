fbf5cc9226ac8afbfd860f5e616d0b44947d1b7b
Leo Valais <leo.valais@epita.fr>
2018-09-12 17:03:31 +0200

    add ohow for a single wiki file

---
+++ src/ohow.ml
@@ -0,0 +1,75 @@
+(* ohow.ml - one html_of_wiki
+
+   Converts a wikicreole file into an HTML file.
+   See README.md for build instructions.
+*)
+
+let compile text = Wiki_syntax.(
+    let par = cast_wp wikicreole_parser in
+    let bi = Wiki_widgets_interface.{
+      bi_page = Site "";
+      bi_sectioning = false;
+      bi_add_link = (fun _ -> ());
+      bi_content = Lwt.return [];
+      bi_title = "";
+    } in
+    Lwt_main.run @@ xml_of_wiki par bi text);;
+
+let build_page content =
+  let ti = match Compiler.extract_h1 content with
+    | Some s -> s
+    | None -> ""
+  in
+  Tyxml.Html.(html
+                (head (title (pcdata ti)) [])
+                (body content));;
+
+let readfile file =
+  let ic = open_in file in
+  let rec readall () =
+    try
+      let line = input_line ic in
+      line :: readall ()
+    with End_of_file -> []
+  in
+  let lines = readall() in
+  close_in ic;
+  String.concat "\n" lines;;
+
+let pprint oc html =
+  let fmt = Format.formatter_of_out_channel oc in
+  Tyxml.Html.pp_elt () fmt html;
+  Format.pp_force_newline fmt ();
+  Format.pp_print_flush fmt ();;
+
+let infer_wiki_name = Filename.remove_extension;;
+let inout_paths file = (file, (infer_wiki_name file) ^ ".html")
+
+let ohow file =
+  let (file_in, file_out) = inout_paths file in
+  let oc = open_out file_out in
+  (file_in
+   |> readfile
+   |> compile
+   |> build_page
+   |> pprint oc);
+  close_out oc;;
+
+let main file =
+  if Sys.file_exists file then
+    ohow file
+  else
+    failwith @@ "no such file: " ^ file;;
+
+let open Cmdliner in
+let file_t =
+  let doc = "The wikicreole file to convert to HTML." in
+  Arg.(required & pos ~rev:true 0 (some string) None & info [] ~docv:"FILE" ~doc)
+in
+let info_ =
+  let doc = "Converts a wikicreole file into an HTML file." in
+  let man = [] in
+  Term.info "ohow" ~version:"v0.0.0" ~doc ~exits:Term.default_exits ~man
+in
+let ohow_t = Term.(const main $ file_t) in
+Term.(exit @@ eval (ohow_t, info_));;
