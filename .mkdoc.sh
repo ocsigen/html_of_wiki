2d1d0fcbbb04e4420fca8861e1f2f06abfd43507
Leo Valais <leo.valais97@gmail.com>
2018-12-11 17:54:01 +0100

    ci: add .travis.yml

---
+++ .mkdoc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+
+# Requires: wget, git, opam, html_of_wiki, jq, comm, find, awk
+
+wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
+bash -ex .travis-ocaml.sh
+
+eval $(opam env)
+
+git clone https://github.com/ocsigen/html_of_wiki.git
+opam pin add -y html_of_wiki html_of_wiki
+
+# wget https://raw.githubusercontent.com/ocsigen/ocsigen.github.io/master/template.wiki 
+
+f=$(mktemp)
+cat >$f <<EOF
+{
+    "api": "api",
+    "menu": true,
+    "templates": ["tmp/template.wiki"],
+    "manual": "manual",
+    "assets": "manual/files",
+    "images": "manual/files"
+}
+EOF
+DEBUG=t quickdop -f doc _doc -t json -c $f -viu
