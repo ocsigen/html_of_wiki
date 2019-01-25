9492498d6caaa3abc53fd8b5cd2cf77655679feb
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-01-25 17:25:19 +0800

    remove some unused files

--- src/bad_bug.ml
+++
@@ -1,6 +0,0 @@
-let () =
-  let fmt = Format.formatter_of_out_channel stdout in
-  let page = Tyxml.Html.(p [pcdata "hello"]) in
-  Tyxml.Html.pp_elt () fmt page;
-  Format.pp_print_flush fmt ()
-  (* haha, not a bug. *)
--- src/eliom_reference.ml
+++
@@ -1,3 +0,0 @@
-type 'a eref
-let get _ = Lwt.return_unit
-let set _ _ = Lwt.return_unit
