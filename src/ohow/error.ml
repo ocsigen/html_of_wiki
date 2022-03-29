f25916f00249fb3ede20713c55f77a5d5839aeca
Hugo Heuzard <hugo.heuzard@nomadic-labs.com>
2022-03-29 16:06:57 +0200

    remove HACK

--- src/ohow/error.ml
+++ src/ohow/error.ml
@@ -10,12 +10,11 @@ let wrap_phrasing name f bi args contents =
   `Phrasing_without_interactive
     (let content =
        try f bi args contents with
-       (* HACK comment references to module Document to avoid circular build *)
-       | Error msg (* as exc *) ->
-         (* bi.Wiki_widgets_interface.bi_add_link (Document.Deadlink exc); *)
+       | Error msg as exc ->
+         bi.Wiki_widgets_interface.bi_add_link (Document.Deadlink exc);
          error (Format.sprintf "Error %s: %s" name msg)
        | exc ->
-         (* bi.Wiki_widgets_interface.bi_add_link (Document.Deadlink exc); *)
+         bi.Wiki_widgets_interface.bi_add_link (Document.Deadlink exc);
          error
            (Format.sprintf "Error %s: exception %s" name
               (Printexc.to_string exc))
